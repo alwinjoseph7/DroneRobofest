5833bbce220563e22d61a4c7915ee7070c111ad0
Adithya M <adithyam866.com>
2021-10-22 16:24:33 +0530

    added qr

---
+++ qr.py
@@ -0,0 +1,14 @@
+import cv2
+
+cap=cv2.VideoCapture(0)
+while True:
+    _,frame = cap.read()
+    objects,_,_=cv2.QRCodeDetector().detectAndDecode(frame)
+    if objects != '':
+        print("Detected")
+    else:
+        print("Not Detected")
+    if cv2.waitKey(1) & 0xFF != ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
