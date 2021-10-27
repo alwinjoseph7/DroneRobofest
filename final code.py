8d44eae166f1c5dbd7381582ff20687d6d6e3b16
Adithya M <adithyam866.com>
2021-10-27 12:31:06 +0530

    added save-clip

--- final code.py
+++ final code.py
@@ -2,6 +2,7 @@ from dronekit import connect, VehicleMode, LocationGlobalRelative
 from pymavlink import mavutil
 import time
 import argparse
+import sys
 import cv2
 
 parser = argparse.ArgumentParser()
@@ -9,6 +10,7 @@ parser.add_argument("--connect", default="/dev/ttyACM0")
 args = parser.parse_args()
 
 cap = cv2.VideoCapture(0)
+clip=cv2.VideoWriter('clip.avi')
 
 # Connect to the Vehicle
 print("Connecting to vehicle on: %s" % args.connect)
@@ -44,56 +46,22 @@ def arm_and_takeoff(aTargetAltitude):
         time.sleep(1)
 
 
-def send_body_ned_velocity(velocity_x, velocity_y, velocity_z, duration=0):
-    msg = vehicle.message_factory.set_position_target_local_ned_encode(
-        0,  # time_boot_ms (not used)
-        0,
-        0,  # target system, target component
-        mavutil.mavlink.MAV_FRAME_BODY_NED,  # frame Needs to be MAV_FRAME_BODY_NED for forward/back left/right control.
-        0b0000111111000111,  # type_mask
-        0,
-        0,
-        0,  # x, y, z positions (not used)
-        velocity_x,
-        velocity_y,
-        velocity_z,  # m/s
-        0,
-        0,
-        0,  # x, y, z acceleration
-        0,
-        0,
-    )
-    for x in range(0, duration):
-        vehicle.send_mavlink(msg)
-        time.sleep(1)
-
-
-# # This is the command to move the copter 5 m/s forward for 10 sec.
-# velocity_x = 0
-# velocity_y = 5
-# velocity_z = 0
-# duration = 10
-# send_body_ned_velocity(velocity_x, velocity_y, velocity_z, duration)
-
-
-# Initialize the takeoff sequence to 5m
 arm_and_takeoff(3)
-print("Take off complete")
+print("Take off complete\n")
 
 while True:
     _, frame = cap.read()
     
     res=cv2.QRCodeDetector().detectAndDecode(frame)
-    if  res=='a':
-        print("Break")
+
+    if  res=='b':
+        print("Break\n")
         break
-    elif res=='b':
-        print("Circle")
-        vehicle.parameters["CIRCLE_RADIUS"] = 200
+
+    elif res=='a':
+        print("Circle\n")
+        vehicle.parameters["CIRCLE_RADIUS"] = 10
         vehicle.mode = VehicleMode("CIRCLE")
-    else: 
-        pass        
-        #person following
 
     if cv2.waitKey(5) & 0xFF == ord("q"):
         break
@@ -101,3 +69,6 @@ while True:
 print("Return to launch")
 vehicle.parameters["RTL_ALT"] = 0
 vehicle.mode = VehicleMode("RTL")
+
+cap.release()
+vehicle.close()
